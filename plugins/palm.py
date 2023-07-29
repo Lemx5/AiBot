e8eb6c3f584c0f357f28646ffba6ee38e38a8a16
ｒｙｍｅ <ryme.care@hotmail.com>
2023-07-29 12:55:42 +0530

    updated

--- plugins/palm.py
+++ plugins/palm.py
@@ -84,8 +84,6 @@ async def default_palm(message):
         'top_k': 40,
         'top_p': 0.95,
         'max_output_tokens': 1024,
-        'stop_sequences': [],
-        'safety_settings': [{"category":"HARM_CATEGORY_DEROGATORY","threshold":1},{"category":"HARM_CATEGORY_TOXICITY","threshold":1},{"category":"HARM_CATEGORY_VIOLENCE","threshold":2},{"category":"HARM_CATEGORY_SEXUAL","threshold":2},{"category":"HARM_CATEGORY_MEDICAL","threshold":2},{"category":"HARM_CATEGORY_DANGEROUS","threshold":2}],
         }
     response = palm.generate_text(
         **defaults,
