b164694f4a4cd165ac1ffab04f21fe8c8e1a6790
ｒｙｍｅ <ryme.care@hotmail.com>
2023-07-29 12:48:35 +0530

    updates

--- database.py
+++ database.py
@@ -24,12 +24,12 @@ class Database:
     async def update_user_context(self, id, context):
         await self.col.update_one({"user_id": id}, {"$set": {"context": context}})
 
-async def get_user_context(self, id):
-    user = await self.col.find_one({"user_id": id})
-    if user and "context" in user:
-        return user["context"]
-    else:
-        return False
+    async def get_user_context(self, id):
+        user = await self.col.find_one({"user_id": id})
+        if user and "context" in user:
+            return user["context"]
+        else:
+            return False
 
     
 db = Database(DATABASE_URL, DATABASE_NAME)
