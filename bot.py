e2a1fd7a5f6d6da1bf885fc7893552429aa93e16
ｒｙｍｅ <ryme.care@hotmail.com>
2023-07-29 12:36:38 +0530

    updated

--- bot.py
+++ bot.py
@@ -1,15 +1,25 @@
-from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
+from pyrogram import Client, __version__
+from info import API_ID, API_HASH, BOT_TOKEN
 import pyromod.listen
 
+class Bot(Client):
 
-app = Client(
-    "palmbot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="plugins"),
-)
+    def __init__(self):
+        super().__init__(
+            "bot",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            plugins={"root": "plugins"},
+        )
 
+    async def start(self):
+        await super().start()
+        print(f"Bot started.")
 
+    async def stop(self, *args):
+        await super().stop()
+        print(f"Bot stopped.")
+
+app = Bot()
 app.run()
