a283270de5c32798039d9ac7352b19913dbbd109
ｒｙｍｅ <ryme.care@hotmail.com>
2023-10-17 22:26:45 +0530

    updated

--- bot.py
+++ bot.py
@@ -168,7 +168,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return "Bot is running!"
 
 def run():
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
