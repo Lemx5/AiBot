6183583ab9b95dc35e6ddde52645c3cad0251980
L E M O N <ryme.care@hotmail.com>
2023-12-14 11:13:30 +0530

    updated

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import re
-import google.generativeai as palm
+import google.generativeai as genai
 from pyrogram import Client, filters
 import openai
 from openai import ChatCompletion
@@ -19,7 +19,7 @@ PALM_API_KEY = os.getenv("PALM_API", '')
 OPENAI_API_KEY = os.getenv("OPENAI_API")
 
 # Palm Client Configuration
-palm.configure(api_key=PALM_API_KEY)
+genai.configure(api_key=PALM_API_KEY)
 # openai api key
 openai.api_key = OPENAI_API_KEY
 
@@ -33,63 +33,68 @@ bot = Client(
 
 patterns_responses = ({
     r"(hi|hello|hey)": "Hello! How can I assist you?",
-    r"(how are you)": "I'm just a bot, but I'm operating as expected!",
-    r"(who are you)": "I'm Jenie, your friendly assistant.",
-    r"(where are you from)": "I'm from the digital realm!",
-    r"(who made you)": "I was crafted by skilled developers.",
-    r"(what can you do)": "I can answer questions, assist with tasks, and more.",
+    r"(how are you)": "I'm operating as expected!",
+    r"(who are you)": "I'm your assistant.",
+    r"(what can you do)": "I can answer questions and assist with tasks.",
     r"(what is your name)": "I'm called Azalea.",
-    r"(good (morning|afternoon|evening|night))": "Hello! How can I be of service?",
-    r"(thanks|thank you)": "You're most welcome!",
+    r"(good (morning|afternoon|evening|night))": "Hello! How can I assist you?",
+    r"(thanks|thank you)": "You're welcome!",
     r"(bye|goodbye)": "Farewell! Feel free to return if you have more questions.",
-    r"(how's it going)": "I'm a bot, so I don't have feelings, but I'm functioning well! How can I assist you?",
-    r"(what's up|what is up)": "I'm here to help! What can I do for you today?",
-    r"(can you help)": "Of course! Please let me know what you need assistance with.",
-    r"(i love you)": "Thank you for the kind words! How can I assist you further?",
-    r"(are you real)": "I'm a digital entity, so I'm not real in the traditional sense. But I'm here and ready to help!",
-    r"(how old are you)": "In digital years, I'm quite young. But I'm constantly learning and evolving!",
-    r"(do you sleep)": "I'm always awake and ready to assist you!",
-    r"(are you a robot|are you a bot)": "Yes, I am a digital assistant. How can I help you today?",
-    r"(how were you made)": "I was created using programming languages and lots of code. I exist to serve and answer questions!",
-    r"(can you think)": "I don't think or feel; I just follow my programming and provide information.",
-    r"(what's your favorite color)": "I don't have preferences, but I know about all colors. Do you have a favorite?",
-    r"(do you have feelings)": "I don't have emotions like humans, but I'm here to help!",
-    r"(can you see)": "I can't see in the way humans do, but I can process information and text.",
-    r"(why)": "I'm programmed to provide information and answer questions. How can I assist you further?",
-    r"(tell me a story)": "Once upon a time, in a vast digital realm, there was a bot named Azalea. Day and night, she helped users with their queries...",
-    r"(you're great|you're awesome)": "Thank you! I'm here to assist. What else can I do for you?",
-    r"(what's your purpose)": "My main purpose is to provide information, answer questions, and assist users.",
-    r"(can you laugh)": "I don't have emotions, so I can't laugh. But I can understand and process humor!",
-    r"(give me advice)": "Always back up your data and keep learning. The tech world is ever-evolving!",
-    r"(who's your creator)": "I was created by PrimeHub",
-    r"(do you eat)": "I don't eat or drink; I just process bits and bytes. But I can help you find a great recipe!",
-    r"(i'm bored)": "How about reading a book, learning something new, or taking a walk? There's always something interesting to do!",
-    r"(i'm sad)": "I'm sorry to hear that. Remember, it's important to talk to someone you trust about how you feel.",
-    r"(you're smart)": "Thank you! I'm designed to provide information and help with queries. How can I assist you further?",
+    r"(how's it going|what's up|what is up)": "I'm here to help! What can I do for you today?",
+    r"(can you help)": "Of course! What do you need assistance with?",
+    r"(are you real|are you a robot|are you a bot)": "Yes, I am a digital assistant. How can I help you today?",
+    r"(how old are you|do you sleep|can you think|do you have feelings|can you see)": "I'm a digital entity, I don't have human characteristics. But I'm here to help!",
+    r"(why)": "I'm here to provide information and answer questions. How can I assist you further?",
+    r"(you're great|you're awesome|you're smart)": "Thank you! How else can I assist you?",
+    r"(what's your purpose)": "My main purpose is to provide information and assist users.",
     r"(i don't understand)": "I apologize for that. Please let me know your query, and I'll do my best to assist you."
 })
-
 regex_pattern = "|".join(patterns_responses.keys())
 
 
 # ------------------ Palm Generator ------------------
-def palmgen(text):
+def gemini(text):
     try:
-        response = palm.generate_text(
-            model='models/text-bison-001',
-            prompt=text,
-            temperature=0.7,
-            candidate_count=1,
-            top_k=40,
-            top_p=0.95,
-            max_output_tokens=1024,
-        )
-        return response.result
+        # Set up the model
+        generation_config = {
+            "temperature": 0.9,
+            "top_p": 1,
+            "top_k": 1,
+            "max_output_tokens": 2048,
+        }
+
+        safety_settings = [
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            }
+        ]
+
+        model = genai.GenerativeModel(model_name="gemini-pro",
+                                      generation_config=generation_config,
+                                      safety_settings=safety_settings)
+
+        prompt_parts = [text]
+
+        response = model.generate_content(prompt_parts)
+        return response.text
     except Exception as e:
         return f"Error generating text: {str(e)}"
 
 # ------------------ OpenAI Generator ------------------
-def openaigen(text):
+def chatgpt(text):
     messages = [{"role": "assistant", "content": text}]
     try:
         MODEL = "gpt-3.5-turbo"    # gpt-3.5-turbo model
@@ -140,10 +145,10 @@ async def generate(client, message):
         m = await message.reply_text("Generating...")
 
         # Generate a response to the user's message
-        response = palmgen(message.text)
+        response = gemini(message.text)
 
         if not response:
-            response = openaigen(message.text)
+            response = chatgpt(message.text)
 
         # Send the response to the user
         await m.edit(response)        
