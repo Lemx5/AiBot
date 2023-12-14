ce8c2a6c324f62a6cd6052acc51b4741e72f7ba7
L E M O N <ryme.care@hotmail.com>
2023-12-14 12:19:13 +0530

    updated

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import re
-import google.generativeai as genai
+import google.generativeai as gemini
 from pyrogram import Client, filters
 from flask import Flask
 from threading import Thread
@@ -15,7 +15,7 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # gemini Client Configuration
-genai.configure(api_key=os.getenv("PALM_API"))
+gemini.configure(api_key=os.getenv("PALM_API"))
 
 # Pyrogram Client Configuration
 bot = Client(
@@ -47,42 +47,10 @@ regex_pattern = "|".join(patterns_responses.keys())
 
 
 # ------------------ Gemini ------------------
-def gemini(text):
+def google(text):
     try:
-
-        generation_config = {
-            "temperature": 0.9,
-            "top_p": 1,
-            "top_k": 1,
-            "max_output_tokens": 2048,
-            }
-
-        safety_settings = [
-          {
-            "category": "HARM_CATEGORY_HARASSMENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-          },
-          {
-            "category": "HARM_CATEGORY_HATE_SPEECH",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-          },
-          {
-            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-          },
-          {
-            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-          }
-        ]
-
-        model = genai.GenerativeModel(model_name="gemini-pro",
-                              generation_config=generation_config,
-                              safety_settings=safety_settings)
-
-        prompt_parts = [text]        
-
-        response = model.generate_content(prompt_parts)
+        model = gemini.GenerativeModel('gemini-pro')        
+        response = model.generate_content(text)
         return response.text
     
     except Exception as e:
@@ -125,7 +93,7 @@ async def generate(client, message):
         m = await message.reply_text("Generating...")
 
         # Generate a response to the user's message
-        response = gemini(message.text)
+        response = google(message.text)
 
         # Send the response to the user
         await m.edit(response)        
